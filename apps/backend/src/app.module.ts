5bca003fcdf8bfb4372ba1f99aa77d92d74c2dcc
Earl Tacogdoy <earltacogdoy@gmail.com>
2026-04-23 05:08:41 +0800

    Initial Commit

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthenticationModule } from './authentication/authentication.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthenticationModule, PrismaModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
